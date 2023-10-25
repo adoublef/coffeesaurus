03e186bda1f72313f45c323dd8aa21062129204b
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-26 00:34:31 +0100

    entrypoint

---
+++ cmd/coffeesaurus/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	q := make(chan os.Signal, 1)
+	signal.Notify(q, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-q
+		cancel()
+	}()
+
+	if err := run(ctx); err != nil {
+		log.Printf("adoublef/coffeesaurus: %s", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) (err error) {
+	mux := chi.NewMux()
+	{
+		mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("ok"))
+		})
+	}
+
+	s := &http.Server{
+		Addr:    ":" + os.Getenv("PORT"),
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	sErr := make(chan error)
+	go func() {
+		sErr <- s.ListenAndServe()
+	}()
+
+	select {
+	case err := <-sErr:
+		return fmt.Errorf("main error: starting server: %w", err)
+	case <-ctx.Done():
+		// TODO
+		return s.Shutdown(context.Background())
+	}
+}
